bbca9f427b7dd762e51cfa0cb3a3b4565760040c
Nicolas Marini <nmarini44@gmail.com>
2018-04-27 22:58:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,14 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+    board.each do |position|
+      if position == "X" || position == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  
